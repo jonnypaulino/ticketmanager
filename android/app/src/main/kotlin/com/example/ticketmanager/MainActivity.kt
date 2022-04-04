ffe1ced2b06a4cd898c877fb5e550c57865c91ab
jonnypaulino <jonnyzico10@gmail.com>
2022-04-04 11:03:42 -0300

    gueis

---
+++ android/app/src/main/kotlin/com/example/ticketmanager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ticketmanager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:ticketmanager/pages/TicketsPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      supportedLocales: [const Locale('pt', 'BR')],
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (context) => TicketsPage(),
+      },
+    );
+  }
+}
