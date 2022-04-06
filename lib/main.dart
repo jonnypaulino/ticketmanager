8c6ea9a763831cc409831d544ad6a8e42a720a06
Elison monteiro <56695160+Elison12@users.noreply.github.com>
2022-04-06 17:00:09 -0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:ticketmanager/pages/TicketsPage.dart';
+import 'package:ticketmanager/pages/match_detail_page.dart';
+import 'package:ticketmanager/pages/tickets_page.dart';
 import 'package:ticketmanager/pages/home.dart';
-import 'package:ticketmanager/pages/CityVsAtlet.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         //home: Scaffold(body: SafeArea(child: TicketsPage())),
         title: 'Champions',
-        initialRoute: '/',
+        initialRoute: '/Home',
         routes: <String, WidgetBuilder>{
           '/': (context) => Home(),
           '/games': (context) => TicketsPage(),
-          '/ticketScreen': (context) => CityVsAtlet(),
+          '/matche_detail_page': (context) => MatchDetails(),
+          // '/CitVsAtl': (context) => CityVsAtlet(),
         });
   }
 }
